f70942a54f3651c00c495a8fe783c4541f6158fa
Gustavo Daniel Britez Alvarenga <115171270+GustavoBritez@users.noreply.github.com>
2023-09-09 02:02:02 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Ejercicio3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_Comenzar = new System.Windows.Forms.Button();
+            this.TX_1 = new System.Windows.Forms.TextBox();
+            this.LT_1 = new System.Windows.Forms.ListBox();
+            this.TX_2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BTN_Comenzar
+            // 
+            this.BTN_Comenzar.Location = new System.Drawing.Point(197, 114);
+            this.BTN_Comenzar.Name = "BTN_Comenzar";
+            this.BTN_Comenzar.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Comenzar.TabIndex = 0;
+            this.BTN_Comenzar.Text = "button1";
+            this.BTN_Comenzar.UseVisualStyleBackColor = true;
+            this.BTN_Comenzar.Click += new System.EventHandler(this.BTN_Comenzar_Click);
+            // 
+            // TX_1
+            // 
+            this.TX_1.Location = new System.Drawing.Point(149, 36);
+            this.TX_1.Name = "TX_1";
+            this.TX_1.Size = new System.Drawing.Size(183, 20);
+            this.TX_1.TabIndex = 1;
+            // 
+            // LT_1
+            // 
+            this.LT_1.FormattingEnabled = true;
+            this.LT_1.Location = new System.Drawing.Point(174, 159);
+            this.LT_1.Name = "LT_1";
+            this.LT_1.Size = new System.Drawing.Size(120, 95);
+            this.LT_1.TabIndex = 2;
+            // 
+            // TX_2
+            // 
+            this.TX_2.Location = new System.Drawing.Point(149, 74);
+            this.TX_2.Name = "TX_2";
+            this.TX_2.Size = new System.Drawing.Size(183, 20);
+            this.TX_2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 335);
+            this.Controls.Add(this.TX_2);
+            this.Controls.Add(this.LT_1);
+            this.Controls.Add(this.TX_1);
+            this.Controls.Add(this.BTN_Comenzar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_Comenzar;
+        private System.Windows.Forms.TextBox TX_1;
+        private System.Windows.Forms.ListBox LT_1;
+        private System.Windows.Forms.TextBox TX_2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio3
+{
+    public partial class Form1 : Form
+    {
+        List<int> caja = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_Comenzar_Click(object sender, EventArgs e)
+        {
+            if ( !string.IsNullOrEmpty(TX_1.Text) && !string.IsNullOrEmpty(TX_2.Text))
+            {
+                if( Int32.TryParse(TX_1.Text , out int num) && Int32.TryParse(TX_2.Text, out int expo))
+                {
+                    potencia(num, expo, caja);
+
+                    foreach ( int n in caja)
+                    {
+                        LT_1.Items.Add(n.ToString());
+                    }
+                    
+                }
+            }
+        }
+
+        private int potencia ( int num , int expo, List<int> Lista)
+        {
+            if( expo > 0)
+            {
+                Lista.Add(num);
+                return potencia( num*2 , expo-1 , Lista);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
